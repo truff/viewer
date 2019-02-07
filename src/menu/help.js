0194b94d6145a063e4fb58bba81745462453a6cf
truff <7673252+truff@users.noreply.github.com>
2019-02-07 13:27:18 -0800

    initial support for displaying pdf help files

--- src/menu/help.js
+++ src/menu/help.js
@@ -2,19 +2,35 @@ const createWindow = require("../helpers/window.js");
 const env = require("../helpers/env.js");
 const path = require("path");
 const url = require("url");
+const {shell} = require("electron");
 
 const helpMenuTemplate = {
     label: "Help",
     submenu: [
         {
-            label: 'NITES-Next Quick Reference Guides',
-            // https://nites-vm-6.sd.spawar.navy.mil/owf/helpFiles
-            click () { 
-                const win = createWindow('help_qrg', {
+            //TODO: Probably good to have a window that allows viewing all help guides also
+            //label: 'NITES-Next Quick Reference Guides',
+            label: 'Help with current page',
+            click () {
+                //TODO(TPR): For now just open the map's help file.  Need to change
+                // so the help doc is the one for the current window.
+                let fullPath = env.nites_host + '/planaTerra/docs/Map.pdf';
+                //shell.openExternal(fullPath);
+                shell.openItem(fullPath);
+                //TODO: Electron native support for PDF is planned to be fixed in version 4
+                // or 5.  Once that happens evaluate whether it is satisfactory and revert
+                // the above to this previous behavior:
+                /*
+                const win = createWindow('help_doc', {
                     width: 850,
-                    height: 600
+                    height: 600,
+                    icon: './some.png',
+                    webPreferences: {
+                        plugins: true
+                    }
                 });
-                win.loadURL(env.nites_host + '/owf/help/ABOUT.html');
+                win.loadURL(fullPath);
+                */
             }
         },
         {
@@ -40,7 +56,7 @@ const helpMenuTemplate = {
                 win.setMenuBarVisibility(false);
                 win.loadURL(
                     url.format({
-                        pathname: path.join(__dirname, "about.html"),
+                        pathname: path.join(__dirname, "..\\about.html"),
                         protocol: "file:",
                         slashes: false
                     })
